7e2fd5512e36652bbe7cf4e112289e67b6854878
Francinilson Rodrigues <58186025+Nilson-Rodrigues@users.noreply.github.com>
2024-09-15 17:13:27 -0300

    atualizando

---
+++ programacao-ludica-master/programacao-ludica-master/scripts/Enum/enums.gd
@@ -0,0 +1,3 @@
+class_name Enum
+
+enum QuestionType {TEXT, AUDIO, VIDEO,IMAGE}
---
+++ programacao-ludica-master/programacao-ludica-master/scripts/resourses/question.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name QuizQuestion
+
+@export var question_info: String
+@export var question_script: String
+@export var Nivel: String
+@export var Questao: String
+@export var type: Enum.QuestionType
+@export var options: Array[String]
+@export var correct: String
+
+
